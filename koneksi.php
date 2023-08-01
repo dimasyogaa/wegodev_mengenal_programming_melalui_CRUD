16af4c057c1cea0ec297e332587a92ce56e86435
Yogadimas <yogadimaspambudi@gmail.com>
2023-08-01 23:22:55 +0700

    Koneksi

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "wegodev_mengenal_crud_toko_buku";
+
+mysqli_connect($server, $user, $password, $database) OR DIE ("koneksi gagal");
